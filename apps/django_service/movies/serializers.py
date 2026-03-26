a8fe8a3fce45c42a7f449b372f592aa972f29f11
futo <fedor.alekseev.msk@gmail.com>
2026-03-26 20:49:37 +0300

    Add WatchlistViewSet with serializer. Includes error 400 validation

--- apps/django_service/movies/serializers.py
+++ apps/django_service/movies/serializers.py
@@ -1,3 +1,19 @@
-"""Serializers for the movies application."""
+from rest_framework import serializers
+from .models import Watchlist
 
-__all__ = []
+
+class WatchlistSerializer(serializers.ModelSerializer):
+    movie_title = serializers.ReadOnlyField(source='movie.title')
+
+    class Meta:
+        model = Watchlist
+        fields = ['id', 'movie', 'movie_title', 'added_at']
+    
+    def validate(self, data):
+        user = self.context['request'].user
+        movie = data.get('movie')
+
+        if Watchlist.objects.filter(user=user, movie=movie).exists():
+            raise serializers.ValidationError("Фильм уже был добавлен.")
+        
+        return data
--- apps/django_service/movies/views.py
+++ apps/django_service/movies/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .models import Watchlist
+from .serializers import WatchlistSerializer
 
-# Create your views here.
+
+class WatchlistViewSet(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        watchlist = Watchlist.objects.filter(user=request.user)
+        serializer = WatchlistSerializer(watchlist, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        movie_id = request.data.get('movie')
+        if Watchlist.objects.filter(user=request.user, movie_id=movie_id).exists():
+            return Response(
+                {"detail": "Фильм уже был добавлен."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = WatchlistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            movie_item = Watchlist.objects.get(pk=pk, user=request.user)
+            movie_item.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Watchlist.DoesNotExist:
+            return Response(
+                {"detail": "Фильм отстутствует в списке."}, 
+                status=status.HTTP_404_NOT_FOUND
+            )
