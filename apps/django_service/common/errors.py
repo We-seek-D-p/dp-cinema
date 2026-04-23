73cb878bb19794c0fbafd3af79b0e655587de119
name no <ympalumpa1000IQ@gmail.com>
2026-04-23 19:57:06 +0300

    refactor(domain): move response logic to DomainError.to_response()

--- apps/django_service/common/errors.py
+++ apps/django_service/common/errors.py
@@ -1,9 +1,20 @@
+from rest_framework.response import Response
+
+
 class DomainError(Exception):
     default_code = "domain_error"
     default_message = "Domain Error"
     http_status_code = 400
 
-    def __init__(self, message=None, code=None, **kwargs):
+    def __init__(self, message=None, code=None, http_status_code=None, **kwargs):
         self.message = message or self.default_message
         self.code = code or self.default_code
+        self.http_status_code = code or self.http_status_code
+        self.kwargs = kwargs
         super().__init__(self.message)
+
+    def to_response(self):
+        data = {'error': {'code': self.code, 'message': self.message}}
+        if self.kwargs:
+            data['error']['details'] = self.kwargs
+        return Response(data, status=self.http_status_code)
